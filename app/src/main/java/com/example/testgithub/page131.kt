5b3acdcde724b9afe935f65d8e6cabac20ec6ec0
F602-06\User <11010131>
2023-11-16 14:49:06 +0800

    add page131

---
+++ app/src/main/java/com/example/testgithub/page131.kt
@@ -0,0 +1,11 @@
+package com.example.testgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page131 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page2)
+    }
+}
